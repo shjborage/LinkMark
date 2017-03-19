81ea687aee3108654b5e145d3850d6ed4a14778c
shjborage <shjborage@gmail.com>
2017-03-19 14:05:55 +0800

    [ADD] add file content and get link

---
+++ Sources/StringExtension.swift
@@ -0,0 +1,31 @@
+//
+//  StringExtension.swift
+//  EchoDemo
+//
+//  Created by shjborage on 19/03/2017.
+//  Copyright © 2017 Saick. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return substring(from: fromIndex)
+    }
+    
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return substring(to: toIndex)
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return substring(with: startIndex..<endIndex)
+    }
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,7 @@
 // overview
 print(CommandLine.arguments)
 
+// parse input
 import CommandLineKit
 let cli = CommandLine()
 let filePath = StringOption(shortFlag: "f", longFlag: "file", required: true, helpMessage: "Path to input file for parsing");
@@ -9,8 +10,67 @@ cli.addOptions(filePath)
 
 do {
     try cli.parse()
-    print("File path is \(filePath.value!)")
 } catch {
     cli.printUsage(error)
+}
+
+// logical
+import Foundation
+
+struct Link: CustomStringConvertible {
+    var depth = 0
+    var text = ""
+
+    var description: String {
+        return "Link with depth:\(depth) text:\(text)"
+    }
+}
+
+func getConentOfFile(_ path: String) -> String {
+    do {
+        let content = try String(contentsOfFile:path, encoding:.utf8)
+        return content;
+    } catch {
+        print("getConentOfFile with error:\(error)")
+        return ""
+    }
+}
+
+struct RegexHelper {
+    let regex: NSRegularExpression
+
+    init(_ pattern: String) throws {
+        regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+    }
+
+    func match(input: String) -> [NSTextCheckingResult] {
+        let matches = regex.matches(in: input, options: [], range: NSMakeRange(0, input.characters.count))
+        return matches
+    }
+}
+
+func getLinks(_ content: String) -> [Link] {
+    do {
+        let regexHelper = try RegexHelper("#+\\s.*")
+        let linksMatch = regexHelper.match(input: content)
+        for linkMatch in linksMatch {
+            let start = linkMatch.range.location
+            let end = linkMatch.range.location + linkMatch.range.length
+            let linkString = content.substring(with: start..<end)
+            print(linkString)
+        }
+    } catch {
+    }
+    return [] 
+}
+
+if let filePathValue = filePath.value {
+    // continue
+    let fileContent = getConentOfFile(filePathValue)
+    let links = getLinks(fileContent)
+    print(links)
+    //print("File path is \(filePathValue), with content : \(fileContent)")
+} else {
+    // end of cli
 }
 
